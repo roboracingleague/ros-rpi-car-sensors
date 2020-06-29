792516fa7e9531e3e374bcd31576bf35d825a7ed
Cristian Nitescu <nitescuc@hotmail.com>
2020-06-29 20:15:56 +0100

    Using Joy message

--- src/remote_reader.cpp
+++ src/remote_reader.cpp
@@ -1,5 +1,5 @@
 #include "ros/ros.h"
-#include "geometry_msgs/Twist.h"
+#include "sensor_msgs/Joy.h"
 
 #include <pigpiod_if2.h>
 
@@ -7,7 +7,10 @@ typedef void (*RemoteReaderCB_t)(double);
 
 class RemoteReader
 {
-    int pi, gpio, cb_id;
+    int pi, gpio, level = 1, cb_id;
+    int range_max = 2000, range_min = 1000;
+    double sensitivity = 0.015;
+    double last_value = 0.0;
     RemoteReaderCB_t callback;
 
     uint32_t start_tick, end_tick;
@@ -21,17 +24,25 @@ class RemoteReader
 
     public:
     
-    RemoteReader(int pi, int gpio, RemoteReaderCB_t callback);
+    RemoteReader(int pi, std::string prefix, RemoteReaderCB_t callback);
+    
+    double getSensitivity();
 };
 
-RemoteReader::RemoteReader(int in_pi, int in_gpio, RemoteReaderCB_t in_callback)
+RemoteReader::RemoteReader(int in_pi, std::string prefix, RemoteReaderCB_t in_callback)
 {
+    ros::NodeHandle pH("~" + prefix);
+
     pi = in_pi;
-    gpio = in_gpio;
+    pH.getParam("gpio", gpio);
+    pH.param("sensitivity", sensitivity, 0.015);
+    pH.param("level", level, 1);
+    pH.param("range_max", range_max, 2000);
+    pH.param("range_min", range_min, 1000);
     callback = in_callback;
 
     set_mode(pi, gpio, PI_INPUT);
-    cb_id = callback_ex(pi, 17, EITHER_EDGE, _callbackExt, (void *)this);
+    cb_id = callback_ex(pi, gpio, EITHER_EDGE, _callbackExt, (void *)this);
 }
 
 void RemoteReader::_callbackExt(int pi, unsigned gpio, unsigned level, uint32_t tick, void *user)
@@ -40,65 +51,76 @@ void RemoteReader::_callbackExt(int pi, unsigned gpio, unsigned level, uint32_t 
       Need a static callback to link with C.
    */
 
-//   ROS_INFO("Into static cb");
-
    RemoteReader *mySelf = (RemoteReader *) user;
 
    mySelf->_callback(level, tick); /* Call the instance callback. */
 }
 
-void RemoteReader::_callback(unsigned level, uint32_t tick)
+void RemoteReader::_callback(unsigned in_level, uint32_t tick)
 {
-//    ROS_INFO("Into _callback");
-    if (level == 1)
+    if (in_level == level)
     {
         start_tick = tick;
     } else {
         end_tick = tick;
         uint32_t diff = end_tick - start_tick;
 
-        callback(remap_value(tick - start_tick));
+        double remaped = remap_value(tick - start_tick);
+        if (fabs(remaped - last_value) > sensitivity)
+        {
+            last_value = remaped;
+            callback(remaped);
+        }
     }
 }
 
 double RemoteReader::remap_value(uint32_t value) 
 {
-    return (value - 1500.0)/500.0;
+    double middle = ((double)(range_max + range_min))/2.0;
+    double amplitude = range_max - middle;
+    return (value - middle)/amplitude;
+}
+
+double RemoteReader::getSensitivity()
+{
+    return sensitivity;
 }
 
 
-
-ros::Publisher remote_drive_publisher;
-double steering, throttle;
+ros::Publisher remote_publisher;
+double steering = 0.0, throttle = 0.0;
+int button_value = 0;
 
 void publish_drive()
 {
-    geometry_msgs::Twist command;
-    command.linear.x = throttle;
-    command.angular.z = steering;
+    sensor_msgs::Joy command;
+    command.axes.resize(2);
+    command.axes[1] = throttle;
+    command.axes[0] = steering;
+    command.buttons.resize(1);
+    command.buttons[0] = button_value;
 
-    remote_drive_publisher.publish(command);
+    remote_publisher.publish(command);
 }
 
 void steering_cb(double value)
 {
-//    ROS_INFO("Into steering_cb");
-    if (fabs(steering - value) > 0.01)
-    {
-        steering = value;
-        publish_drive();
-    }
+    steering = value;
+    publish_drive();
 }
 
 void throttle_cb(double value)
 {
-//    ROS_INFO("Into steering_cb");
-    if (fabs(throttle - value) > 0.01)
-    {
-        throttle = value;
-        publish_drive();
-    }
+    throttle = value;
+    publish_drive();
 }
+
+void channel3_cb(double value)
+{
+    button_value = (int)value;
+    publish_drive();
+}
+
 
 int main(int argc, char** argv)
 {
@@ -109,14 +131,14 @@ int main(int argc, char** argv)
     steering = 0.0;
     throttle = 0.0;
 
-    remote_drive_publisher = n.advertise<geometry_msgs::Twist>("/remote/drive", 1);
+    remote_publisher = n.advertise<sensor_msgs::Joy>("/remote", 1);
 
     // Init GPIO
     int pi = pigpio_start(NULL, NULL);
 
-//    ROS_INFO("Create reader");
-    RemoteReader steering(pi, 17, steering_cb);
-    RemoteReader throttle(pi, 27, throttle_cb);
+    RemoteReader steering(pi, "steering", steering_cb);
+    RemoteReader throttle(pi, "throttle", throttle_cb);
+    RemoteReader ch3(pi, "channel3", channel3_cb);
 
     // Handle ROS communication events
     ros::spin();
